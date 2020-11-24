f3eb664afe4a007f124d059cfc7f79e891e4a401
siyual-park <siyual.bak@gmail.com>
2020-11-24 19:50:09 +0900

    _1_1_hello_window.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#![cfg_attr(feature = "cargo-clippy", allow(cast_lossless))]
+
+extern crate gl;
+
+#[cfg(feature = "chapter-1")]
+mod _1_getting_started;
+#[cfg(feature = "chapter-1")]
+use _1_getting_started::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Call with the number of the tutorial, e.g. `1_1_2` for _1_2_hello_window_clear.rs");
+        std::process::exit(1);
+    }
+    let tutorial_id = &args[1];
+
+    match tutorial_id.as_str() {
+        #[cfg(feature = "chapter-1")] "1_1_1" => main_1_1_1(),
+        _     => println!("Unknown tutorial id")
+    }
 }
